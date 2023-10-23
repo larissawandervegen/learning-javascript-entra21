2507402c82aa5110e385f162d1b2f3cf665756e0
Larissa Wandervegen <lari.wandervegen@gmail.com>
2023-10-23 15:29:01 -0300

    Delete nomeAno.js

--- nomeAno.js
+++
@@ -1,5 +0,0 @@
-let nome = 'Larissa';
-let anoDeNascimento = 2005;
-let anoAtual = new Date().getFullYear();
-let idade = anoAtual - anoDeNascimento;
-console.log('A idade de', nome, 'é igual a', idade);
