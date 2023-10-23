282a500f27a92b86aa8e2c03fc606559ec6edd17
Larissa Wandervegen <lari.wandervegen@gmail.com>
2023-10-23 15:28:04 -0300

    Delete verificarMaiorIdade.js

--- verificarMaiorIdade.js
+++
@@ -1,8 +0,0 @@
-let idade = 1;
-if (idade >= 18){
-    console.log('Você tem ', idade, 'pode beber cerveja!');
-}else if (idade <= 2){
-    console.log('Você tem',idade, 'só pode beber leite!');
-}else{
-    console.log('Você tem', idade, 'só pode beber suco!');
-}
